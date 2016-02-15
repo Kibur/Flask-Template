58d0bdb731719650aad960436e3a062f12eb02aa
Kibur <manu2293@hotmail.com>
2016-02-15 16:12:34 +0100

    Basic bootstrap and structural changes

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -6,4 +6,4 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 
-from hello_world import views
+from hello_world import views, errors
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,11 +1,7 @@
 __author__ = 'Kibur'
 
-import logging
 from flask import render_template
 from hello_world import app
-
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
 
 @app.route('/')
 def index():
@@ -16,19 +12,6 @@ def index():
 def hello_world(username=None):
     return render_template('hellow.html', name=username)
 
-@app.errorhandler(403)
-def forbidden(error):
-    return render_template('errors/403.html'), 403
-
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('errors/404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_errors(error):
-    return render_template('errors/500.html'), 500
-
-@app.errorhandler(Exception)
-def all_exceptions_handler(error):
-    log.exception('Unhandled exception')
-    return render_template('errors/500.html'), 500
+@app.route('/about/')
+def about():
+    return render_template('about.html')
